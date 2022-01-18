a6541daceb22f34e758d1a6f044ac27f48a9b3cf
younes rivandi <ryounes69@gmail.com>
2022-01-18 16:17:50 +0330

    ok svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
 /** @type {import('@sveltejs/kit').Config} */
-// import adapterStatic from "@sveltejs/adapter-static";
+import adapterStatic from "@sveltejs/adapter-static";
 const config = {
-    // kit: {
+    kit: {
         // hydrate the <div id="svelte"> element in src/app.html
         target: '#svelte',
-        adapter: "@sveltejs/adapter-static",
-    // }
+        adapter: adapterStatic({fallback: 'app.html'}),
+    }
 };
 
 export default config;
