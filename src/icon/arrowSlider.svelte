46f566f3a668844c2ad5ee2a7b95e5951f5ac6e5
younes rivandi <ryounes69@gmail.com>
2022-01-18 14:49:32 +0330

    next

--- src/icon/arrowSlider.svelte
+++
@@ -1,6 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="9.251" height="15.674" viewBox="0 0 9.251 15.674">
-    <g id="Group_151" data-name="Group 151" transform="translate(1.414 1.414)">
-        <line id="Line_40" data-name="Line 40" x2="6.423" y2="6.423" fill="none" stroke="#4fb1ff" stroke-linecap="round" stroke-width="2"/>
-        <line id="Line_41" data-name="Line 41" x1="6.423" y2="6.423" transform="translate(0 6.423)" fill="none" stroke="#4fb1ff" stroke-linecap="round" stroke-width="2"/>
-    </g>
-</svg>
